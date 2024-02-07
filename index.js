0696eb3e2c1a5f6ca35d0fc7d8b12db5fa579e52
josephsamimmanuel <josesamimmanuel@gmail.com>
2024-02-07 20:33:01 +0530

    Updated

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const bclicked = document.getElementById("btn");
 const bmi = document.getElementById("bmi-result");
 const weightCondition = document.getElementById("weight-condition");
 
+
+
 bclicked.addEventListener("click", calculateBMI);
 
-const heightValue = document.getElementById("height").value / 100;  //height in meter
-const weightValue = document.getElementById("weight").value;
-const bmiValue = weightValue / (heightValue * heightValue);         //bmivalue
 
-function calculateBMI() {                                           //After butoon clicked
-  bmi.value = bmiValue;                                             //Bmi value
+function calculateBMI() {                                         
+  const heightValue = document.getElementById("height").value / 100;         //height in meter
+  const weightValue = document.getElementById("weight").value;              
+  const bmiValue = weightValue / (heightValue * heightValue);                // bmi=weight(kg/(height)2)
+bmi.value = bmiValue;                                             
 
-  if (bmiValue < 18.5) {                                            //weight condition
+  if (bmiValue < 18.5) {                                            
     weightCondition.innerText = "Under weight";
   } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
     weightCondition.innerText = "Normal weight";
